079d57d6806575b359e2f5be4ceb74b49b9be3c3
Lizhanhong <3012187388@qq.com>
2026-03-17 16:16:36 +0800

    Nacos注册

---
+++ services/service-product/src/main/java/com/lzh/product/ProductMainApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient//启用服务注册与发现
+@SpringBootApplication
+public class ProductMainApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProductMainApplication.class,args);
+    }
+}
